90be21bcb3a9d279567dc2cb3eb2b7e8af27d5e7
Shawn Stratton <sstratton@drud.com>
2019-11-19 02:07:17 -0500

    Fixing env vars

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import {ParseTags} from "./parser";
 
 async function run() {
     try {
-        const head = process.env['GITHUB_HEAD'];
-        const sha = process.env['GITHUB_REF'];
+        const head = process.env['GITHUB_REF'];
+        const sha = process.env['GITHUB_SHA'];
         console.log(`Head ${head} and sha ${sha}`);
         core.setOutput('tag', ParseTags(head, sha));
     } catch (error) {
