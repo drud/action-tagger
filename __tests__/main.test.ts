a4f5e7a036547f99a6c4c27e83a03d0eaa61d7e8
Shawn Stratton <sstratton@drud.com>
2019-11-19 01:41:15 -0500

    Better test coverage

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,7 +3,7 @@ import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
-test('Gets Correct Ref', async() => {
+test('Gets Correct Ref',  () => {
    let input = [{
       head: "refs/tags/v1.0.0",
       sha: "2aab82bas23bD",
@@ -15,7 +15,12 @@ test('Gets Correct Ref', async() => {
    }];
 
    input.forEach( (element) => {
-      expect(ParseTags(element.head, element.sha)).toBe(element.expect);
+      expect( ParseTags(element.head, element.sha)).toBe(element.expect);
    });
 
 });
+
+test('Check for bad Value', () => {
+   expect(() => ParseTags('$GITHUB_REF', '$GITHUB_HEAD')).toThrowError();
+});
+
--- src/parser.ts
+++ src/parser.ts
@@ -1,7 +1,13 @@
 export function ParseTags(head: string, sha: string) {
     let tagPath = "refs/tags/";
-    if (head && 0 !== head.length && head != "nil" && head.startsWith(tagPath)) {
-        return head.substr(tagPath.length, head.length);
+    //Catch bad conditions
+    if (head && 0 !== head.length && head != "nil") {
+        if ( head.startsWith(tagPath)) {
+            return head.substr(tagPath.length, head.length);
+        }
+    }
+    if (head.startsWith('$GITHUB') || sha.startsWith('$GITHUB')) {
+        throw new Error('Value of Sha and/or Head contain litteral references to the GITHUB environment var names, not values');
     }
     return sha.substr(0, 6);
 }
